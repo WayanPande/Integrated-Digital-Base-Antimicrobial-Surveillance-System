d5efe548b424be4d5bd6d9daef7df64ca2b6bc0e
WayanPande <yande554@gmail.com>
2022-09-07 02:05:23 +0800

    add tambah antibiotik button and dialog

--- lib/screens/add_patien_antibiotik_screen.dart
+++ lib/screens/add_patien_antibiotik_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:multi_select_flutter/chip_field/multi_select_chip_field.dart';
 import 'package:multi_select_flutter/dialog/multi_select_dialog_field.dart';
 import 'package:multi_select_flutter/util/multi_select_item.dart';
+import 'package:project_pak_gusan/util/data_drop_down.dart';
 
 import '../util/data_class.dart';
 
@@ -12,7 +14,11 @@ class AddPatienAntibiotik extends StatefulWidget {
 }
 
 class _AddPatienAntibiotikState extends State<AddPatienAntibiotik> {
-  bool _diberikanAntibiotik = false, _kombinasiAntibiotik = false;
+  bool _diberikanAntibiotik = false,
+      _kombinasiAntibiotik = false,
+      _antibiotikLain = false,
+      _reaksiAlergi = false,
+      _efekSamping = false;
 
   final List<String> jalurPemberian = [
     "Oral",
@@ -21,7 +27,7 @@ class _AddPatienAntibiotikState extends State<AddPatienAntibiotik> {
     "Parental",
     "Topikal"
   ];
-  String _selectedJalurPemberian = "Oral";
+  String _selectedJalurPemberian = "Oral", _selectedNamaAntibiotik = "Aldesulfone sodium";
 
   static List<Data> namaAntibiotik = [
     Data(id: 1, name: "Ingus"),
@@ -32,6 +38,29 @@ class _AddPatienAntibiotikState extends State<AddPatienAntibiotik> {
   final _itemsNamaAntibiotik = namaAntibiotik
       .map((data) => MultiSelectItem<Data>(data, data.name))
       .toList();
+
+  Future<void> _showMyDialog() async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Tambah antibiotik'),
+          content: SingleChildScrollView(
+            child: DialogContent(),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Tambah'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -167,106 +196,49 @@ class _AddPatienAntibiotikState extends State<AddPatienAntibiotik> {
                   ),
                 ],
               ),
-              const SizedBox(
-                height: 30,
-              ),
-              const Text(
-                "Nama Antibiotik",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              MultiSelectDialogField(
-                items: _itemsNamaAntibiotik,
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.black38,
-                    width: 1,
-                  ),
-                  borderRadius: BorderRadius.circular(15),
-                ),
-                title: const Text(
-                  "Nama Antibiotik",
-                ),
-                buttonIcon: const Icon(
-                  Icons.arrow_drop_down,
-                ),
-                selectedColor: const Color(0xFF20BDB7),
-                onConfirm: (index) {},
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              const Text(
-                "Jalur Pemberian",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              DecoratedBox(
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.black38,
-                    width: 1,
-                  ),
-                  borderRadius: BorderRadius.circular(15),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 15,
-                    vertical: 5,
-                  ),
-                  child: DropdownButton(
-                    value: _selectedJalurPemberian,
-                    items: jalurPemberian
-                        .map((code) =>
-                            DropdownMenuItem(value: code, child: Text(code)))
-                        .toList(),
-                    onChanged: (index) {
-                      setState(() {
-                        _selectedJalurPemberian = index.toString();
-                      });
-                    },
-                    isExpanded: true,
-                    underline: Container(),
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              TextFormField(
-                decoration: const InputDecoration(
-                  suffixText: "mg",
-                  labelText: 'Dosis',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(15),
+              Visibility(
+                visible: _kombinasiAntibiotik,
+                child: Column(
+                  children: [
+                    const SizedBox(
+                      height: 15,
                     ),
-                  ),
-                ),
-                keyboardType: TextInputType.number,
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              TextFormField(
-                decoration: const InputDecoration(
-                  suffixText: "Hari",
-                  labelText: 'Lama Pemberian',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(15),
+                    MultiSelectChipField(
+                      items: _itemsNamaAntibiotik,
+
+                      onTap: (values) {
+
+                      },
+                      title: Text("Nama antibiotik"),
                     ),
-                  ),
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {
+                            _showMyDialog();
+                          },
+                          style: ElevatedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(14),
+                            ),
+                            padding: const EdgeInsets.symmetric(
+                                vertical: 15, horizontal: 15),
+                          ),
+                          child: const Text(
+                            "Tambah antibiotik",
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ],
                 ),
-                keyboardType: TextInputType.number,
               ),
               const SizedBox(
                 height: 30,
@@ -286,11 +258,11 @@ class _AddPatienAntibiotikState extends State<AddPatienAntibiotik> {
                     children: [
                       Radio(
                         value: true,
-                        groupValue: _kombinasiAntibiotik,
+                        groupValue: _antibiotikLain,
                         activeColor: const Color(0xFF20BDB7),
                         onChanged: (index) {
                           setState(() {
-                            _kombinasiAntibiotik = true;
+                            _antibiotikLain = true;
                           });
                         },
                       ),
@@ -306,11 +278,11 @@ class _AddPatienAntibiotikState extends State<AddPatienAntibiotik> {
                     children: [
                       Radio(
                         value: false,
-                        groupValue: _kombinasiAntibiotik,
+                        groupValue: _antibiotikLain,
                         activeColor: const Color(0xFF20BDB7),
                         onChanged: (index) {
                           setState(() {
-                            _kombinasiAntibiotik = false;
+                            _antibiotikLain = false;
                           });
                         },
                       ),
@@ -339,11 +311,11 @@ class _AddPatienAntibiotikState extends State<AddPatienAntibiotik> {
                     children: [
                       Radio(
                         value: true,
-                        groupValue: _kombinasiAntibiotik,
+                        groupValue: _reaksiAlergi,
                         activeColor: const Color(0xFF20BDB7),
                         onChanged: (index) {
                           setState(() {
-                            _kombinasiAntibiotik = true;
+                            _reaksiAlergi = true;
                           });
                         },
                       ),
@@ -359,11 +331,11 @@ class _AddPatienAntibiotikState extends State<AddPatienAntibiotik> {
                     children: [
                       Radio(
                         value: false,
-                        groupValue: _kombinasiAntibiotik,
+                        groupValue: _reaksiAlergi,
                         activeColor: const Color(0xFF20BDB7),
                         onChanged: (index) {
                           setState(() {
-                            _kombinasiAntibiotik = false;
+                            _reaksiAlergi = false;
                           });
                         },
                       ),
@@ -374,20 +346,102 @@ class _AddPatienAntibiotikState extends State<AddPatienAntibiotik> {
                   ),
                 ],
               ),
+              Visibility(
+                visible: _reaksiAlergi,
+                child: Column(
+                  children: [
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        labelText: 'Deskripsi reaksi yang muncul',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(15),
+                          ),
+                        ),
+                      ),
+                      maxLines: 4,
+                      keyboardType: TextInputType.streetAddress,
+                    ),
+                  ],
+                ),
+              ),
               const SizedBox(
                 height: 30,
               ),
-              TextFormField(
-                decoration: const InputDecoration(
-                  labelText: 'Deskripsi Efek Samping',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(15),
-                    ),
-                  ),
+              const Text(
+                "Apakah ada efek samping",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
                 ),
-                maxLines: 4,
-                keyboardType: TextInputType.streetAddress,
+              ),
+              const SizedBox(
+                height: 12,
+              ),
+              Row(
+                children: [
+                  Row(
+                    children: [
+                      Radio(
+                        value: true,
+                        groupValue: _efekSamping,
+                        activeColor: const Color(0xFF20BDB7),
+                        onChanged: (index) {
+                          setState(() {
+                            _efekSamping = true;
+                          });
+                        },
+                      ),
+                      const Text(
+                        "Ya",
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    width: 15,
+                  ),
+                  Row(
+                    children: [
+                      Radio(
+                        value: false,
+                        groupValue: _efekSamping,
+                        activeColor: const Color(0xFF20BDB7),
+                        onChanged: (index) {
+                          setState(() {
+                            _efekSamping = false;
+                          });
+                        },
+                      ),
+                      const Text(
+                        "Tidak",
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              Visibility(
+                visible: _efekSamping,
+                child: Column(
+                  children: [
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        labelText: 'Deskripsi efek samping yang muncul',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(15),
+                          ),
+                        ),
+                      ),
+                      maxLines: 4,
+                      keyboardType: TextInputType.streetAddress,
+                    ),
+                  ],
+                ),
               ),
               const SizedBox(
                 height: 30,
@@ -430,3 +484,145 @@ class _AddPatienAntibiotikState extends State<AddPatienAntibiotik> {
     );
   }
 }
+
+
+class DialogContent extends StatefulWidget {
+  const DialogContent({Key? key}) : super(key: key);
+
+  @override
+  State<DialogContent> createState() => _DialogContentState();
+}
+
+class _DialogContentState extends State<DialogContent> {
+
+  String _selectedJalurPemberian = "Oral", _selectedNamaAntibiotik = "Aldesulfone sodium";
+  final List<String> jalurPemberian = [
+    "Oral",
+    "IV",
+    "IM",
+    "Parental",
+    "Topikal"
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const SizedBox(
+          height: 30,
+        ),
+        const Text(
+          "Nama Antibiotik",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        DecoratedBox(
+          decoration: BoxDecoration(
+            border: Border.all(
+              color: Colors.black38,
+              width: 1,
+            ),
+            borderRadius: BorderRadius.circular(15),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 15,
+              vertical: 5,
+            ),
+            child: DropdownButton(
+              value: _selectedNamaAntibiotik,
+              items: dataAntibiotik
+                  .map((code) =>
+                  DropdownMenuItem(value: code, child: Text(code)))
+                  .toList(),
+              onChanged: (index) {
+                setState(() {
+                  _selectedNamaAntibiotik = index.toString();
+                });
+              },
+              isExpanded: true,
+              underline: Container(),
+            ),
+          ),
+        ),
+        const SizedBox(
+          height: 30,
+        ),
+        const Text(
+          "Jalur Pemberian",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        DecoratedBox(
+          decoration: BoxDecoration(
+            border: Border.all(
+              color: Colors.black38,
+              width: 1,
+            ),
+            borderRadius: BorderRadius.circular(15),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 15,
+              vertical: 5,
+            ),
+            child: DropdownButton(
+              value: _selectedJalurPemberian,
+              items: jalurPemberian
+                  .map((code) =>
+                  DropdownMenuItem(value: code, child: Text(code)))
+                  .toList(),
+              onChanged: (index) {
+                setState(() {
+                  _selectedJalurPemberian = index.toString();
+                });
+              },
+              isExpanded: true,
+              underline: Container(),
+            ),
+          ),
+        ),
+        const SizedBox(
+          height: 30,
+        ),
+        TextFormField(
+          decoration: const InputDecoration(
+            suffixText: "mg",
+            labelText: 'Dosis',
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.all(
+                Radius.circular(15),
+              ),
+            ),
+          ),
+          keyboardType: TextInputType.number,
+        ),
+        const SizedBox(
+          height: 30,
+        ),
+        TextFormField(
+          decoration: const InputDecoration(
+            suffixText: "Hari",
+            labelText: 'Lama Pemberian',
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.all(
+                Radius.circular(15),
+              ),
+            ),
+          ),
+          keyboardType: TextInputType.number,
+        ),
+      ],
+    );
+  }
+}
+
--- lib/util/data_class.dart
+++ lib/util/data_class.dart
@@ -7,3 +7,16 @@ class Data {
     required this.name,
   });
 }
+
+class Antibiotik {
+  final int id, dosis, lamaPemberian;
+  final String nama, jalurPemberian;
+
+  Antibiotik({
+    required this.id,
+    required this.dosis,
+    required this.lamaPemberian,
+    required this.nama,
+    required this.jalurPemberian,
+  });
+}
